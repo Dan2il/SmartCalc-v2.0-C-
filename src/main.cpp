3d60e87208ade60625c7f9677563b5180719b682
Windhelm Melessa <windhelm@su-f5.msk.21-school.ru>
2024-02-28 20:48:50 +0300

    Мелкие правки

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 
 int main() {
   s21::Model model;
-  std::string expression = "(2+2)-1*3/xmod7-(5-5)-cos(1) + sin(3)*tan(111111111)/ log(X) - (acos(5)*asin(654321)    *      atan(-54) / sqrt(16 + ln(5)) ) * (2 ^ 2)";
+  std::string expression =
+      "(2+2)-1*3/xmod7-(5-5)-cos(1) + sin(3)*tan(111111111)/ log(X) - "
+      "(acos(5)*asin(654321)    *      atan(-54) / sqrt(16 + ln(5)) ) * (2 ^ "
+      "2)";
   model.ConvertStrExpressionToList(expression);
   expression.clear();
   model.PrintInfix();
